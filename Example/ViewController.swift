ab0df2385808c5b3512301bd46cd03e413cb86d2
gamesPlayer <liwj0510@126.com>
2016-03-22 10:21:27 +0800

    now it support iOS7 by following the instruction of @robertjpayne on the github repositoriy of SnapKit

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -12,7 +12,6 @@ class ViewController: UIViewController {
 
     var newbox = UIView()
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,15 +19,30 @@ class ViewController: UIViewController {
         newbox.backgroundColor = UIColor.greenColor()
         self.view.addSubview(newbox)
         
-//        newbox.snp_makeConstraints { (make) -> Void in
-//            make.edges.equalTo(self.view).inset(UIEdgeInsetsMake(20, 20, 20, 20))
-//        }
-        
-        // Either the code above or below have the same issue: Value of type 'UIView' has no member 'snp_makeConstraints'
-        
         newbox.snp_makeConstraints { (make) -> Void in
-            make.width.height.equalTo(50)
-            make.center.equalTo(self.view)
+            make.edges.equalTo(self.view).inset(UIEdgeInsetsMake(20, 20, 20, 20))
+        }
+        
+        UIView.animateWithDuration(6, animations: { () -> Void in
+            self.newbox.backgroundColor = UIColor.redColor()
+            
+            self.newbox.snp_removeConstraints()
+            
+            self.newbox.snp_makeConstraints { (make) -> Void in
+                make.edges.equalTo(self.view).inset(UIEdgeInsetsMake(80, 80, 80, 80))
+            }
+            
+            }) { (finished) -> Void in
+                if finished{
+                    self.newbox.snp_removeConstraints()
+                    self.newbox.snp_makeConstraints(closure: { (make) -> Void in
+                        
+                        make.width.height.equalTo(200)
+                        make.center.equalTo(self.view)
+                        
+                        self.newbox.backgroundColor = UIColor.yellowColor()
+                    })
+                }
         }
         
     }
